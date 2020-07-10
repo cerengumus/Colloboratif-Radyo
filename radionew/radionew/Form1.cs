6baffd65bf045028b3a8f90abe74a341d4085698
cerengumus <32721239+cerengumus@users.noreply.github.com>
2020-07-10 16:09:06 +0300

    Add files via upload

---
+++ radionew/radionew/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radionew
+{
+    public partial class music_player : Form
+    {
+        public music_player()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            for(int i = 0; i < openFileDialog1.SafeFileNames.Length; i++)
+            {
+                listBox1.Items.Add(openFileDialog1.SafeFileNames[i].ToString());
+                listBox2.Items.Add(openFileDialog1.FileNames[i].ToString());
+            }
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.SelectedIndex = listBox1.SelectedIndex;
+            axWindowsMediaPlayer1.URL = listBox2.SelectedItem.ToString();
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume += 10;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume -= 10;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.settings.volume = trackBar1.Value;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.fastForward();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.fastReverse();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
